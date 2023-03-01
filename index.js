545074a6f38bde7b097adf63126acf47ffb0db37
DhanushChinnaiyan <dhanushmd4021@gmail.com>
2023-03-01 23:16:09 +0530

    c

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ import express from "express";
 import {studentsRouter} from "./student.js"
 import cors from 'cors';
 import { teachersRouter } from "./teacher.js";
+import { client } from "./db.js";
 
 
 
@@ -21,6 +22,14 @@ app.use(express.static("express")); // loading the static file
 app.use(express.json()) // middleware tells server to use json
 app.use(cors())
 
+
+app.get("/",async(req,res)=>{
+    const data = await client
+    .db("mongoworkout")
+    .showCollections()
+
+    res.status(200).json({routes:data})
+})
 app.use("/students" , studentsRouter)
 app.use("/teachers" , teachersRouter)
 app.listen(9000,()=>console.log(`server started localhost:9000`))
